af2d24f52c99ce17e040fbd5d74282f195d0680c
Cooper Bartlett <cooperbar@gmail.com>
2021-04-07 22:44:19 +1000

    Initial Cuffs plugin

---
+++ cuffs.py
@@ -0,0 +1,52 @@
+import logging
+import pwnagotchi.plugins as plugins
+
+
+class Cuffs(plugins.Plugin):
+    __author__ = 'idoloninmachina@gmail.com'
+    __version__ = '0.0.1'
+    __license__ = 'GPL3'
+    __description__ = 'Measure the amount of time taken by the pwnagotchi to capture a handshake'
+
+    def __init__(self):
+        logging.debug("[Cuffs] Cuffs plugin created")
+
+    def on_loaded(self):
+        logging.info("[Cuffs] Plugin loading")
+
+        if 'whitelist' not in self.options:
+            self.options['whitelist'] = list()
+
+        logging.info("[Cuffs] Plugin loaded")
+
+    def on_unload(self, ui):
+        logging.info("[Cuffs] Plugin unloaded")
+
+    def on_unfiltered_ap_list(self, agent, access_points):
+        count = 0
+        for ap in access_points:
+            if ap not in self.options['whitelist']:
+                access_points.remove(ap)
+                count += 1
+
+        if (count > 0):
+            logging.info(f"[Cuffs] Removed {count} unrestricted ap's")
+
+    def on_wifi_update(self, agent, access_points):
+        for ap in access_points:
+            if ap not in self.options['whitelist']:
+                logging.error(
+                    "[Cuffs] Cuffs is enabled, yet an unrestricted ap has made it past our filter.")
+                logging.debug(f"Unrestricted AP: {ap}")
+
+    def on_deauthentication(self, agent, access_point, client_station):
+        if access_point not in self.options['whitelist']:
+            logging.error(
+                "[Cuffs] Cuffs is enabled, yet an unrestricted ap has made it past our filter and has been deauthenticated.")
+            logging.debug(f"Unrestricted AP: {ap}")
+
+    def on_association(self, agent, access_point):
+        if access_point not in self.options['whitelist']:
+            logging.error(
+                "[Cuffs] Cuffs is enabled, yet an unrestricted ap has made it past our filter and has been assosciated.")
+            logging.debug(f"Unrestricted AP: {ap}")
